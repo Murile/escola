118f2f1fc86bcf9275e217edd5cfa9eaa6266536
Rodrigo <rodrigo.anjos0806@gmail.com>
2022-11-04 03:31:58 -0300

    bom dia

--- src/page/Qrcode/index.js
+++ src/page/Qrcode/index.js
@@ -1,8 +1,18 @@
 import './index.scss'
+import Header from '../../components/header';
 
 export default function Qrcode() {
     return(
-        <div>saghsa</div>
+        <main className='qr-container'>
+            <Header />
+            <section className='qrcode'>
+                <img src="assets/img/alemao.png" alt="" />
+                <div>
+                    <h1>qrcode pika</h1>
+                    <img src="assets/img/prete.jpg" alt="" />
+                </div>
+            </section>
+        </main>
     );
     
 }
